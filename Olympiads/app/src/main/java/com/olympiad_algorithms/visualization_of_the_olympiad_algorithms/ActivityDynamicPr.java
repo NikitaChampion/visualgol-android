d2d021a0e72fab399225ac36a7ddeba067903dba
Nikita <nikitaigumnov2002@yandex.ru>
2018-04-01 01:04:05 +0300

    Added first code

---
+++ Olympiads/app/src/main/java/com/olympiad_algorithms/visualization_of_the_olympiad_algorithms/ActivityDynamicPr.java
@@ -0,0 +1,117 @@
+package com.olympiad_algorithms.visualization_of_the_olympiad_algorithms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityDynamicPr extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonPosled;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_pr);
+
+        buttonPosled = (Button) findViewById(R.id.buttonPosled);
+        buttonPosled.setOnClickListener(this);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
+        button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
+        button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(this);
+
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+
+        button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonPosled:
+                Intent intent = new Intent(this, ActivityPosled.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                //Intent intent = new Intent(this, .class);
+                //startActivity(intent);
+                break;
+            case R.id.button3:
+                //Intent intent = new Intent(this, .class);
+                //startActivity(intent);
+                break;
+            case R.id.button4:
+                //Intent intent = new Intent(this, .class);
+                //startActivity(intent);
+                break;
+            case R.id.button5:
+                //Intent intent = new Intent(this, .class);
+                //startActivity(intent);
+                break;
+            case R.id.button6:
+                //Intent intent = new Intent(this, .class);
+                //startActivity(intent);
+                break;
+            case R.id.button7:
+                //Intent intent = new Intent(this, .class);
+                //startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
