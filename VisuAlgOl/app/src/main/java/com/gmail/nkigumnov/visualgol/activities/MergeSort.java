30cc9864cc091289a5b03273e8a698fbffed03df
Nikita Igumnov <nkigumnov@edu.hse.ru>
2021-02-27 00:49:14 +0300

    Update merge sort

--- VisuAlgOl/app/src/main/java/com/gmail/nkigumnov/visualgol/activities/MergeSort.java
+++ VisuAlgOl/app/src/main/java/com/gmail/nkigumnov/visualgol/activities/MergeSort.java
@@ -14,6 +14,7 @@ import com.gmail.nkigumnov.visualgol.util.Constants;
 import com.gmail.nkigumnov.visualgol.util.Util;
 
 import java.util.Random;
+import java.util.Timer;
 
 public class MergeSort extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
     private EditText editText;
@@ -23,8 +24,9 @@ public class MergeSort extends AppCompatActivity implements View.OnClickListener
     private final int[] array = new int[8];
     private final TextView[] txtNum = new TextView[8];
     private TextView[] mergeArray = new TextView[8];
-    private Merge mergeSort;
     private final Random random = new Random();
+    private Merge timerAction;
+    private Timer sort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +48,6 @@ public class MergeSort extends AppCompatActivity implements View.OnClickListener
         txtNum[6] = findViewById(R.id.txt_num7);
         txtNum[7] = findViewById(R.id.txt_num8);
 
-        mergeArray = new TextView[8];
         mergeArray[0] = findViewById(R.id.arr1);
         mergeArray[1] = findViewById(R.id.arr2);
         mergeArray[2] = findViewById(R.id.arr3);
@@ -59,6 +60,11 @@ public class MergeSort extends AppCompatActivity implements View.OnClickListener
         tvDelay = findViewById(R.id.tv_delay);
         tvDelay.setText(R.string.sec);
 
+        findViewById(R.id.previous).setOnClickListener(this);
+        findViewById(R.id.next).setOnClickListener(this);
+        findViewById(R.id.stop).setOnClickListener(this);
+        findViewById(R.id.continue_).setOnClickListener(this);
+
         ((SeekBar) findViewById(R.id.sb_delay)).setOnSeekBarChangeListener(this);
         findViewById(R.id.sort).setOnClickListener(this);
         findViewById(R.id.generate).setOnClickListener(this);
@@ -67,25 +73,64 @@ public class MergeSort extends AppCompatActivity implements View.OnClickListener
         editText = findViewById(R.id.edit_text);
 
         generate();
-        mergeSort = new Merge(this, array, curSpeed);
+        sort = new Timer();
+        timerAction = new Merge(this, array, -1);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.sort) {
-            mergeSort.interrupt();
+            sort.cancel();
             update();
-            mergeSort = new Merge(this, array, curSpeed);
-            mergeSort.start();
+            sort = new Timer();
+            timerAction = new Merge(this, array, -1);
+            sort.scheduleAtFixedRate(timerAction, 0, curSpeed);
         } else if (id == R.id.generate) {
-            mergeSort.interrupt();
-            update();
+            sort.cancel();
             generate();
+            timerAction = new Merge(this, array, -1);
+            sort = new Timer();
         } else if (id == R.id.btn_save) {
             Util.saveText(this, ((editText.getText().toString().equals("1 3 4 4 5 6 4 5")) ? '1' : '0'),
                     groupPosition + childPosition);
+        } else if (id == R.id.previous) {
+            previous();
+        } else if (id == R.id.next) {
+            next();
+        } else if (id == R.id.stop) {
+            stop();
+        } else if (id == R.id.continue_) {
+            continue_();
         }
+    }
+
+    private void previous() {
+        sort.cancel();
+        sort = new Timer();
+        if (timerAction.timerCounter <= 0) {
+            return;
+        }
+        timerAction.timerCounter -= 2;
+        timerAction.run();
+    }
+
+    private void next() {
+        sort.cancel();
+        sort = new Timer();
+        timerAction.run();
+    }
+
+    private void stop() {
+        sort.cancel();
+        sort = new Timer();
+    }
+
+    private void continue_() {
+        sort.cancel();
+        sort = new Timer();
+        timerAction = new Merge(this, array, timerAction.timerCounter);
+        sort.scheduleAtFixedRate(timerAction, 0, curSpeed);
     }
 
     private void generate() {
@@ -96,46 +141,26 @@ public class MergeSort extends AppCompatActivity implements View.OnClickListener
     }
 
     private void update() {
-        runOnUiThread(() -> {
-            for (int i = 0; i < array.length; ++i) {
-                txtNum[i].setText(String.valueOf(array[i]));
-                txtNum[i].setBackgroundResource(R.drawable.rectangle_gray);
-                mergeArray[i].setText(" ");
-                mergeArray[i].setBackgroundResource(R.drawable.rectangle_white);
-            }
-        });
+        for (int i = 0; i < array.length; ++i) {
+            txtNum[i].setText(String.valueOf(array[i]));
+            txtNum[i].setBackgroundResource(R.drawable.rectangle_gray);
+            mergeArray[i].setText("");
+            mergeArray[i].setBackgroundResource(R.drawable.rectangle_white);
+        }
     }
 
-    public void setColor(final int[] indices, final int color) {
-        runOnUiThread(() -> {
-            for (int index : indices) {
-                txtNum[index].setBackgroundResource(color);
-            }
-        });
+    public void setColor(final int[] colors1, final int[] colors2) {
+        for (int i = 0; i < array.length; ++i) {
+            txtNum[i].setBackgroundResource(colors1[i]);
+            mergeArray[i].setBackgroundResource(colors2[i]);
+        }
     }
 
-    public void setMergeColor(final int[] indices, final int color) {
-        runOnUiThread(() -> {
-            for (int index : indices) {
-                mergeArray[index].setBackgroundResource(color);
-            }
-        });
-    }
-
-    public void setText(final int[] indices, final String[] text) {
-        runOnUiThread(() -> {
-            for (int i = 0; i < indices.length; ++i) {
-                txtNum[indices[i]].setText(text[i]);
-            }
-        });
-    }
-
-    public void setMergeText(final int[] indices, final String[] text) {
-        runOnUiThread(() -> {
-            for (int i = 0; i < indices.length; ++i) {
-                mergeArray[indices[i]].setText(text[i]);
-            }
-        });
+    public void setText(final String[] values1, final String[] values2) {
+        for (int i = 0; i < array.length; ++i) {
+            txtNum[i].setText(values1[i]);
+            mergeArray[i].setText(values2[i]);
+        }
     }
 
     @Override
@@ -155,3 +180,4 @@ public class MergeSort extends AppCompatActivity implements View.OnClickListener
 
     }
 }
+
--- VisuAlgOl/app/src/main/java/com/gmail/nkigumnov/visualgol/algorithms/sorting/Merge.java
+++ VisuAlgOl/app/src/main/java/com/gmail/nkigumnov/visualgol/algorithms/sorting/Merge.java
@@ -1,110 +1,154 @@
 package com.gmail.nkigumnov.visualgol.algorithms.sorting;
 
 import android.app.Activity;
+import android.util.Pair;
 
 import com.gmail.nkigumnov.visualgol.R;
 import com.gmail.nkigumnov.visualgol.activities.MergeSort;
 
-public class Merge extends Thread {
-    private final Activity activity;
-    private final int[] array;
-    private final int speed;
+import java.util.TimerTask;
 
-    public Merge(Activity activity, int[] array, int speed) {
+public class Merge extends TimerTask {
+    private final Activity activity;
+    private final int[] mainArray;
+    public int timerCounter;
+
+    public Merge(Activity activity, int[] array, int timerCounter) {
         this.activity = activity;
-        this.array = array.clone();
-        this.speed = speed;
+        mainArray = array.clone();
+        this.timerCounter = timerCounter;
     }
 
     @Override
     public void run() {
-        try {
-            sort();
-        } catch (InterruptedException ignored) {
-        }
+        Pair<Pair<int[], int[]>, Pair<String[], String[]>> p = sort();
+        ((MergeSort) activity).setColor(p.first.first, p.first.second);
+        ((MergeSort) activity).setText(p.second.first, p.second.second);
+        ++timerCounter;
     }
 
-    private void merge(int l, int mid, int r) throws InterruptedException {
-        Thread.sleep(speed);
+    private int merge(int l, int mid, int r, int[] colors, int[] colorsMerge, int[] array,
+                      String[] textArray, String[] textArrayMerge, int currentTime) {
         for (int i = 0; i < array.length; ++i) {
             if (i >= l && i < r) {
-                ((MergeSort) activity).setColor(new int[]{i}, R.drawable.rectangle_gray);
+                colors[i] = R.drawable.rectangle_gray;
             } else {
-                ((MergeSort) activity).setColor(new int[]{i}, R.drawable.rectangle_2_gray);
+                colors[i] = R.drawable.rectangle_2_gray;
             }
+        }
+        if (currentTime++ == timerCounter) {
+            return currentTime;
         }
         int i1 = 0, i2 = 0;
         int[] result = new int[r - l];
         while (l + i1 < mid && mid + i2 < r) {
-            Thread.sleep(speed);
-            ((MergeSort) activity).setColor(new int[]{l + i1}, R.drawable.rectangle_orange);
-            ((MergeSort) activity).setColor(new int[]{mid + i2}, R.drawable.rectangle_orange);
+            colors[l + i1] = R.drawable.rectangle_orange;
+            colors[mid + i2] = R.drawable.rectangle_orange;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
+            }
             if (array[l + i1] < array[mid + i2]) {
                 result[i1 + i2] = array[l + i1];
-                Thread.sleep(speed);
-                ((MergeSort) activity).setText(new int[]{l + i1},
-                        new String[]{""});
-                ((MergeSort) activity).setMergeText(new int[]{l + i1 + i2},
-                        new String[]{Integer.toString(array[l + i1])});
-                ((MergeSort) activity).setColor(new int[]{l + i1}, R.drawable.rectangle_gray);
+
+                textArray[l + i1] = "";
+                textArrayMerge[l + i1 + i2] = Integer.toString(array[l + i1]);
+                colors[l + i1] = R.drawable.rectangle_gray;
+                if (currentTime++ == timerCounter) {
+                    return currentTime;
+                }
                 ++i1;
             } else {
                 result[i1 + i2] = array[mid + i2];
-                Thread.sleep(speed);
-                ((MergeSort) activity).setText(new int[]{mid + i2},
-                        new String[]{""});
-                ((MergeSort) activity).setMergeText(new int[]{l + i1 + i2},
-                        new String[]{Integer.toString(array[mid + i2])});
-                ((MergeSort) activity).setColor(new int[]{mid + i2}, R.drawable.rectangle_gray);
+
+                textArray[mid + i2] = "";
+                textArrayMerge[l + i1 + i2] = Integer.toString(array[mid + i2]);
+                colors[mid + i2] = R.drawable.rectangle_gray;
+                if (currentTime++ == timerCounter) {
+                    return currentTime;
+                }
                 ++i2;
             }
         }
         while (l + i1 < mid) {
-            Thread.sleep(speed);
-            ((MergeSort) activity).setColor(new int[]{l + i1}, R.drawable.rectangle_orange);
+            colors[l + i1] = R.drawable.rectangle_orange;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
+            }
             result[i1 + i2] = array[l + i1];
-            Thread.sleep(speed);
-            ((MergeSort) activity).setText(new int[]{l + i1},
-                    new String[]{""});
-            ((MergeSort) activity).setMergeText(new int[]{l + i1 + i2},
-                    new String[]{Integer.toString(array[l + i1])});
-            ((MergeSort) activity).setColor(new int[]{l + i1}, R.drawable.rectangle_gray);
+
+            textArray[l + i1] = "";
+            textArrayMerge[l + i1 + i2] = Integer.toString(array[l + i1]);
+            colors[l + i1] = R.drawable.rectangle_gray;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
+            }
             ++i1;
         }
         while (mid + i2 < r) {
-            Thread.sleep(speed);
-            ((MergeSort) activity).setColor(new int[]{mid + i2}, R.drawable.rectangle_orange);
+            colors[mid + i2] = R.drawable.rectangle_orange;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
+            }
             result[i1 + i2] = array[mid + i2];
-            Thread.sleep(speed);
-            ((MergeSort) activity).setText(new int[]{mid + i2},
-                    new String[]{""});
-            ((MergeSort) activity).setMergeText(new int[]{l + i1 + i2},
-                    new String[]{Integer.toString(array[mid + i2])});
-            ((MergeSort) activity).setColor(new int[]{mid + i2}, R.drawable.rectangle_gray);
+
+            textArray[mid + i2] = "";
+            textArrayMerge[l + i1 + i2] = Integer.toString(array[mid + i2]);
+            colors[mid + i2] = R.drawable.rectangle_gray;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
+            }
             ++i2;
         }
         for (int i = 0; i < i1 + i2; ++i) {
             array[l + i] = result[i];
-            Thread.sleep(speed);
-            ((MergeSort) activity).setMergeColor(new int[]{l + i}, R.drawable.rectangle_orange);
+            colorsMerge[l + i] = R.drawable.rectangle_orange;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
+            }
 
-            Thread.sleep(speed);
-            ((MergeSort) activity).setMergeText(new int[]{l + i}, new String[]{" "});
-            ((MergeSort) activity).setText(new int[]{l + i},
-                    new String[]{Integer.toString(array[l + i])});
-            ((MergeSort) activity).setMergeColor(new int[]{l + i}, R.drawable.rectangle_white);
-        }
-    }
-
-    private void sort() throws InterruptedException {
-        for (int i = 1; i < array.length; i *= 2) {
-            for (int j = 0; j < array.length - i; j += 2 * i) {
-                merge(j, j + i, Math.min(j + 2 * i, array.length));
+            textArray[l + i] = Integer.toString(array[l + i]);
+            textArrayMerge[l + i] = "";
+            textArrayMerge[l + i] = "";
+            colorsMerge[l + i] = R.drawable.rectangle_white;
+            if (currentTime++ == timerCounter) {
+                return currentTime;
             }
         }
-        Thread.sleep(speed);
+        return currentTime;
+    }
+
+    private Pair<Pair<int[], int[]>, Pair<String[], String[]>> sort() {
+        int[] colors = new int[mainArray.length];
+        int[] colorsMerge = new int[mainArray.length];
+        int[] array = mainArray.clone();
+        String[] textArray = new String[mainArray.length];
+        String[] textArrayMerge = new String[mainArray.length];
+        int currentTime = -1;
         for (int i = 0; i < array.length; ++i) {
-            ((MergeSort) activity).setColor(new int[]{i}, R.drawable.rectangle_dark);
+            colors[i] = R.drawable.rectangle_gray;
+            colorsMerge[i] = R.drawable.rectangle_white;
+            textArray[i] = String.valueOf(array[i]);
+            textArrayMerge[i] = "";
         }
+        if (currentTime++ == timerCounter) {
+            return new Pair<>(new Pair<>(colors, colorsMerge), new Pair<>(textArray, textArrayMerge));
+        }
+        for (int i = 1; i < array.length; i *= 2) {
+            for (int j = 0; j < array.length - i; j += 2 * i) {
+                currentTime = merge(j, j + i, Math.min(j + 2 * i, array.length), colors, colorsMerge, array,
+                        textArray, textArrayMerge, currentTime);
+                if (currentTime == timerCounter + 1) {
+                    return new Pair<>(new Pair<>(colors, colorsMerge), new Pair<>(textArray, textArrayMerge));
+                }
+            }
+        }
+        for (int i = 0; i < array.length; ++i) {
+            colors[i] = R.drawable.rectangle_dark;
+        }
+        if (currentTime == timerCounter) {
+            return new Pair<>(new Pair<>(colors, colorsMerge), new Pair<>(textArray, textArrayMerge));
+        }
+        --timerCounter;
+        return new Pair<>(new Pair<>(colors, colorsMerge), new Pair<>(textArray, textArrayMerge));
     }
 }
